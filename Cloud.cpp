db507635fe4a265a8de47118f9cbce1e5afa3c9b
xuanthai2 <68328065+xuanthai2@users.noreply.github.com>
2021-11-14 22:06:59 +0700

    Update: + fix boundingbox of some floors + bounding box goomba vs first floor + koopa walking left right animation - the world 1 - 1 black backround near portal

--- Cloud.cpp
+++ Cloud.cpp
@@ -48,7 +48,7 @@ void CCloud::RenderBoundingBox()
 void CCloud::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	float cellWidth_div_2 = this->cellWidth / 2;
-	l = x - 2 - cellWidth_div_2;
+	l = x  - cellWidth_div_2;
 	t = y - 2 - this->cellHeight / 2;
 	r = l + this->cellWidth * this->length;
 	b = t + this->cellHeight;
--- Platform.cpp
+++ Platform.cpp
@@ -50,7 +50,7 @@ void CPlatform::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	float cellWidth_div_2 = this->cellWidth / 2;
 	l = x - cellWidth_div_2;
-	t = y - this->cellHeight / 2;
+	t = y  - this->cellHeight / 2;
 	r = l + this->cellWidth * this->length;
 	b = t + this->cellHeight;
 }
